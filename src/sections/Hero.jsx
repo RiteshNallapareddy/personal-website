0b6c0eae506662d96fc69680506bed0fcce0a285
Ritesh <riteshpr8@gmail.com>
2026-04-12 02:57:25 -0400

    feat: add typewriter animation to Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,11 +1,32 @@
 import '../styles/Hero.css'
+import { useState, useEffect } from 'react'
+
 
 function Hero() {
+  const roles = ["Quant Developer.", "Engineer.", "Builder."]
+    const [roleIndex, setRoleIndex] = useState(0)
+    const [displayText, setDisplayText] = useState('')    
+
+  useEffect(() => {
+  const currentRole = roles[roleIndex]
+  
+  if (displayText.length < currentRole.length) {
+    setTimeout(() => {
+      setDisplayText(currentRole.slice(0, displayText.length + 1))
+    }, 100)
+  } else {
+    setTimeout(() => {
+      setRoleIndex((roleIndex + 1) % roles.length)
+      setDisplayText('')
+    }, 2000)
+  }
+}, [displayText, roleIndex])
+
   return (
     <div className="hero">
   <div className="hero-content">
     <h1 className="hero-name">Ritesh Nallapareddy</h1>
-    <h2 className="hero-title">Aspiring Quant Developer</h2>
+    <h2 className="hero-title">{displayText}</h2>
     <div className="hero-buttons">
       <a href="#projects" className="btn-primary">View Projects</a>
       <a href="#contact" className="btn-secondary">Contact Me</a>
